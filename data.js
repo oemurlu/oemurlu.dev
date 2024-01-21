46177866f7c663dfe2e5c35296a0e416293f7089
Enes Kaya <eneskaya5261@gmail.com>
2024-01-21 09:15:11 +0300

    feat: create swiper for projects part

---
+++ data.js
@@ -0,0 +1,37 @@
+// you can take background's values from this website "https://gradients.shecodes.io/gradients"
+
+const swiperData = [
+    {
+      title: "Dergice",
+      logo: "/assets/dergice/dergiCE-logo.png",
+      images: ["/assets/dergice/ss1.png", "/assets/dergice/ss2.png", "/assets/dergice/ss3.png"],
+      appType: "App Store",
+      appLink: "https://apps.apple.com/tr/app/dergice/id6475283036",
+      background: "linear-gradient(to right, #00dbde 0%, #fc00ff 100%)"
+    },
+    {
+      title: "Food Order App",
+      logo: "/assets/foodOrder/food-order-logo.png",
+      images: ["/assets/foodOrder/ss1.png", "/assets/foodOrder/ss2.png", "/assets/foodOrder/ss3.png"],
+      appType: "Github",
+      appLink: "https://github.com/oemurlu/Food-Order-App-MVVM-Repository-Firebase",
+      background: "linear-gradient(to right, #ec77ab 0%, #7873f5 100%)"
+    },
+    {
+      title: "Movie World",
+      logo: "/assets/movieFinder/movie-world-logo.png",
+      images: ["/assets/movieFinder/ss1.png", "/assets/movieFinder/ss2.png", "/assets/movieFinder/ss3.png"],
+      appType: "Github",
+      appLink: "https://github.com/oemurlu/Movie-World-MVVM-MovieAPP",
+      background: "linear-gradient(to right, rgb(47, 81, 158) 5.5%, rgb(242, 75, 164) 92.8%)"
+    },
+    {
+      title: "BazaarBundle - eCommerce app",
+      logo: "/assets/bazaarBundle/bazaarBundle-logo.png",
+      images: ["/assets/bazaarBundle/ss1.png", "/assets/bazaarBundle/ss2.png", "/assets/bazaarBundle/ss3.png"],
+      appType: "Github",
+      appLink: "https://github.com/oemurlu/BazaarBundle_E-Commerce-Shopping-App",
+      background: "linear-gradient(to right, rgb(219, 55, 53) 5.5%, rgb(177, 14, 236) 92.8%)"
+    },
+    // other swiper values...
+  ];
