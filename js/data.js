4bc923d284620e0716d9d0ac3ea0a13a4c3254dc
oemurlu <oemurlu@gmail.com>
2024-01-31 11:21:44 +0300

    add video games app and update cv

--- js/data.js
+++ js/data.js
@@ -2,15 +2,24 @@
 
 const swiperData = [
     {
+      title: "Video Games",
+      logo: "/assets/videoGames/video-games-logo.png",
+      images: ["/assets/videoGames/ss1.png", "/assets/videoGames/ss2.png", "/assets/videoGames/ss3.png"],
+      appType: "Github",
+      appLink: "https://github.com/oemurlu/Video-Games-App-MVVM-ProgrammaticUI",
+      background: "linear-gradient(to left, #FF3CAC 0%, #2B86C5 70%)"
+
+    },
+    {
       title: "dergiCE",
       logo: "/assets/dergice/dergiCE-logo.png",
       images: ["/assets/dergice/ss1.png", "/assets/dergice/ss2.png", "/assets/dergice/ss3.png"],
       appType: "App Store",
       appLink: "https://apps.apple.com/tr/app/dergice/id6475283036",
-      background: "linear-gradient(to right, #00dbde 0%, #fc00ff 100%)"
+      background: "linear-gradient(to right, #FF3CAC 0%, #2B86C5 80%)"
     },
     {
-      title: "Food Order App",
+      title: "Food Order",
       logo: "/assets/foodOrder/food-order-logo.png",
       images: ["/assets/foodOrder/ss1.png", "/assets/foodOrder/ss2.png", "/assets/foodOrder/ss3.png"],
       appType: "Github",
@@ -23,7 +32,7 @@ const swiperData = [
       images: ["/assets/movieFinder/ss1.png", "/assets/movieFinder/ss2.png", "/assets/movieFinder/ss3.png"],
       appType: "Github",
       appLink: "https://github.com/oemurlu/Movie-World-MVVM-MovieAPP",
-      background: "linear-gradient(to right, rgb(47, 81, 158) 5.5%, rgb(242, 75, 164) 92.8%)"
+      background: "linear-gradient(to right, rgb(47, 81, 158) 40%, rgb(242, 75, 164) 92.8%)"
     },
     {
       title: "BazaarBundle",
