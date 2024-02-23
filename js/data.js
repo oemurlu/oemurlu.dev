3fcc3a5dcb86b3bcd8492585c28d48c6ac862661
Enes Kaya <eneskaya5261@gmail.com>
2024-02-24 00:15:23 +0300

    create key features part of slide

--- js/data.js
+++ js/data.js
@@ -1,46 +1,50 @@
 // you can take background's values from this website "https://gradients.shecodes.io/gradients"
 
 const swiperData = [
-    {
-      title: "Video Games",
-      logo: "/assets/videoGames/video-games-logo.png",
-      images: ["/assets/videoGames/ss1.png", "/assets/videoGames/ss2.png", "/assets/videoGames/ss3.png"],
-      appType: "Github",
-      appLink: "https://github.com/oemurlu/Video-Games-App-MVVM-ProgrammaticUI",
-      background: "linear-gradient(to left, #FF3CAC 0%, #2B86C5 70%)"
-
-    },
-    {
-      title: "dergiCE",
-      logo: "/assets/dergice/dergiCE-logo.png",
-      images: ["/assets/dergice/ss1.png", "/assets/dergice/ss2.png", "/assets/dergice/ss3.png"],
-      appType: "App Store",
-      appLink: "https://apps.apple.com/tr/app/dergice/id6475283036",
-      background: "linear-gradient(to right, #FF3CAC 0%, #2B86C5 80%)"
-    },
-    {
-      title: "Food Order",
-      logo: "/assets/foodOrder/food-order-logo.png",
-      images: ["/assets/foodOrder/ss1.png", "/assets/foodOrder/ss2.png", "/assets/foodOrder/ss3.png"],
-      appType: "Github",
-      appLink: "https://github.com/oemurlu/Food-Order-App-MVVM-Repository-Firebase",
-      background: "linear-gradient(to right, #ec77ab 0%, #7873f5 100%)"
-    },
-    {
-      title: "Movie World",
-      logo: "/assets/movieFinder/movie-world-logo.png",
-      images: ["/assets/movieFinder/ss1.png", "/assets/movieFinder/ss2.png", "/assets/movieFinder/ss3.png"],
-      appType: "Github",
-      appLink: "https://github.com/oemurlu/Movie-World-MVVM-MovieAPP",
-      background: "linear-gradient(to right, rgb(47, 81, 158) 40%, rgb(242, 75, 164) 92.8%)"
-    },
-    {
-      title: "BazaarBundle",
-      logo: "/assets/bazaarBundle/bazaarBundle-logo.png",
-      images: ["/assets/bazaarBundle/ss1.png", "/assets/bazaarBundle/ss2.png", "/assets/bazaarBundle/ss3.png"],
-      appType: "Github",
-      appLink: "https://github.com/oemurlu/BazaarBundle_E-Commerce-Shopping-App",
-      background: "linear-gradient(to right, rgb(219, 55, 53) 5.5%, rgb(177, 14, 236) 92.8%)"
-    },
-    // other swiper values...
-  ];
+  {
+    title: "Video Games",
+    logo: "/assets/videoGames/video-games-logo.png",
+    images: ["/assets/videoGames/ss1.png", "/assets/videoGames/ss2.png", "/assets/videoGames/ss3.png"],
+    appType: "Github",
+    appLink: "https://github.com/oemurlu/Video-Games-App-MVVM-ProgrammaticUI",
+    background: "linear-gradient(to left, #FF3CAC 0%, #2B86C5 70%)",
+    keyFeatures: ["MVVM", "Programmatic UI", "CoreData", "Pagination", "General Network Layer"]
+  },
+  {
+    title: "dergiCE",
+    logo: "/assets/dergice/dergiCE-logo.png",
+    images: ["/assets/dergice/ss1.png", "/assets/dergice/ss2.png", "/assets/dergice/ss3.png"],
+    appType: "App Store",
+    appLink: "https://apps.apple.com/tr/app/dergice/id6475283036",
+    background: "linear-gradient(to right, #FF3CAC 0%, #2B86C5 80%)",
+    keyFeatures: ["Webview", "OneSignal", "StoreKit", "App Store processes"]
+  },
+  {
+    title: "Food Order",
+    logo: "/assets/foodOrder/food-order-logo.png",
+    images: ["/assets/foodOrder/ss1.png", "/assets/foodOrder/ss2.png", "/assets/foodOrder/ss3.png"],
+    appType: "Github",
+    appLink: "https://github.com/oemurlu/Food-Order-App-MVVM-Repository-Firebase",
+    background: "linear-gradient(to right, #ec77ab 0%, #7873f5 100%)",
+    keyFeatures: ["MVVM Repository Pattern", "Firebase", "Alamofire", "Generic Network Layer"]
+  },
+  {
+    title: "Movie World",
+    logo: "/assets/movieFinder/movie-world-logo.png",
+    images: ["/assets/movieFinder/ss1.png", "/assets/movieFinder/ss2.png", "/assets/movieFinder/ss3.png"],
+    appType: "Github",
+    appLink: "https://github.com/oemurlu/Movie-World-MVVM-MovieAPP",
+    background: "linear-gradient(to right, rgb(47, 81, 158) 40%, rgb(242, 75, 164) 92.8%)",
+    keyFeatures: ["MVVM", "Firebase", "Alamofire", "Generic Network Layer"]
+  },
+  {
+    title: "BazaarBundle",
+    logo: "/assets/bazaarBundle/bazaarBundle-logo.png",
+    images: ["/assets/bazaarBundle/ss1.png", "/assets/bazaarBundle/ss2.png", "/assets/bazaarBundle/ss3.png"],
+    appType: "Github",
+    appLink: "https://github.com/oemurlu/BazaarBundle_E-Commerce-Shopping-App",
+    background: "linear-gradient(to right, rgb(219, 55, 53) 5.5%, rgb(177, 14, 236) 92.8%)",
+    keyFeatures: ["MVC", "Firebase", "Alamofire"]
+  },
+  // other swiper values...
+];
